1d6f6d7abc65b798487ad00a24179a97c9a6eccc
mosharrof <mosharrofhossain8413@gmail.com>
2024-06-28 20:23:18 +0600

    MiniProject Alhamdulillah done

--- MiniPorject_1/index.js
+++ MiniPorject_1/index.js
@@ -1,2 +1,19 @@
 const slideshowElements=document.querySelectorAll(".slideshow-element");
-console.log(slideshowElements);
+// console.log(slideshowElements);
+let cntElement=1;
+setInterval(()=>
+{
+    cntElement++;
+    let currentElement=document.querySelector(".current");
+    currentElement.classList.remove("current");
+    if(cntElement>slideshowElements.length)
+        {
+            slideshowElements[0].classList.add("current");
+            cntElement=1;
+        }
+        else 
+        {
+            currentElement.nextElementSibling.classList.add("current");
+        }
+
+},2000)
